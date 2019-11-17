af5b65c7203ac965e1fc6eea84ebd3291ab35789
Andrii.S <skor_av@ukr.net>
2019-11-17 11:35:26 +0200

    added the icons required to put the app to the new Android Play Market

--- build.cmd
+++ build.cmd
@@ -7,5 +7,6 @@ ionic cordova run android --device -l --debug
 # build
 ionic cordova build android --release --prod
 "%java_home%\bin\jarsigner" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ukc-release-key.keystore c:\Users\Andrii\ionic\ukc\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk ukc
-del c:\Users\Andrii\ionic\ukc\platforms\android\app\build\outputs\apk\release\ukc.apk
-"%android_sdk_root%\build-tools\29.0.2\zipalign" -v 4 c:\Users\Andrii\ionic\ukc\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk c:\Users\Andrii\ionic\ukc\platforms\android\app\build\outputs\apk\release\ukc.apk
+del c:\Users\Andrii\ionic\ukc\platforms\android\app\build\outputs\apk\release\1545.apk
+"%android_sdk_root%\build-tools\29.0.2\zipalign" -v 4 c:\Users\Andrii\ionic\ukc\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk c:\Users\Andrii\ionic\ukc\platforms\android\app\build\outputs\apk\release\1545.apk
+"%android_sdk_root%\build-tools\29.0.2\apksigner" verify c:\Users\Andrii\ionic\ukc\platforms\android\app\build\outputs\apk\release\1545.apk
